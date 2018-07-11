7f6dd4c2cbaf4862de027335c8cea23ba0a8d7be
Ananya Phadke <phadkeananya@gmail.com>
2018-07-11 19:31:09 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -3,16 +3,22 @@ def least_coins(cents)
 if cents/25 > 0
   quarters = cents/25
   cents = cents % 25
+else
+  quarters = 0
 end
 
 if cents/10 > 0
   dimes = cents/10
   cents = cents % 10
+else
+  dimes = 0
 end
 
 if cents/5 > 0 
   nickels = cents/5
   cents = cents % 5
+else
+  nickels = 0
 end
 
 
